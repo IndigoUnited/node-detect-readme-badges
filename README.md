8ec7749996b7d5e0d3f3f04bb962ef7bf058e34e
carsy <jlageb@gmail.com>
2016-02-11 01:30:12 +0000

    Init commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# node-detect-repo-badges
+
+> Scans a repository's readme file, searching for badges
+
+[![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Coverage Status][coveralls-image]][coveralls-url] [![Dependency status][david-dm-image]][david-dm-url] [![Dev Dependency status][david-dm-dev-image]][david-dm-dev-url]
+
+[npm-url]:https://npmjs.org/package/detect-repo-badges
+[npm-image]:http://img.shields.io/npm/v/detect-repo-badges.svg?style=flat-square
+[travis-url]:https://travis-ci.org/IndigoUnited/node-detect-repo-badges
+[travis-image]:http://img.shields.io/travis/IndigoUnited/node-detect-repo-badges.svg?branch=master&style=flat-square
+[coveralls-url]:https://coveralls.io/r/IndigoUnited/node-detect-repo-badges
+[coveralls-image]:https://img.shields.io/coveralls/IndigoUnited/node-detect-repo-badges.svg?branch=master&style=flat-square
+[david-dm-url]:https://david-dm.org/IndigoUnited/node-detect-repo-badges
+[david-dm-image]:https://img.shields.io/david/IndigoUnited/node-detect-repo-badges.svg?style=flat-square
+[david-dm-dev-url]:https://david-dm.org/IndigoUnited/node-detect-repo-badges#info=devDependencies
+[david-dm-dev-image]:https://img.shields.io/david/dev/IndigoUnited/node-detect-repo-badges.svg?style=flat-square
+
+## Tests
+
+`$ npm test`
+
+## License
+
+Released under the [MIT License](http://www.opensource.org/licenses/mit-license.php).
---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const parseUrl = require('url').parse;
+const getUrls = require('get-urls');
+const requireDirectory = require('require-directory');
+const parsers = requireDirectory(module, './lib', { recurse: false });
+
+// npm downloads https://img.shields.io/npm/${dm || dt}/${package}.svg
+// npm version https://img.shields.io/npm/v/${package}.svg
+// nodei.co https://nodei.co/npm/${package}.svg
+// travis-ci https://travis-ci.org/${user}/${package}.svg?branch=${branch}
+// appveyor https://ci.appveyor.com/api/projects/status/${hash}?svg=true
+// circle-ci https://circleci.com/gh/${user}/${package}.svg
+// david https://david-dm.org/${user}/${package}.svg
+// david dev https://david-dm.org/${user}/${package}/dev-status.svg
+// codacy https://api.codacy.com/project/badge/grade/${hash}
+// coveralls https://coveralls.io/github/${user}/${package}.svg?branch=${branch}
+// gitter https://badges.gitter.im/${user}/${package}.png
+
+// maybe codeship/parallelci
+// https://codecov.io/github/codecov/codecov-ruby/coverage.svg?branch=master
+// http://www.coverity.com/
+
+// default to shields.io
+
+function isBadgeUrl(url) {
+    return ['.svg', '.png', '.jpg', '.gif', 'svg=true', 'png=true', '/badge/']
+    .some(extension => url.indexOf(extension) !== -1);
+}
+
+// Separate urls by http first
+function extractUrls(readme) {
+    return getUrls(readme.split('http').map(url => 'http' + url).join(' '))
+    .map(url => url && url.split(')')[0]) // ignore markdown syntax
+    .filter(url => !!url);
+}
+
+function parseBadgeUrl(url) {
+    const parsedUrl = parseUrl(url);
+    const badgeParser = Object.keys(parsers)
+        .find(badgeParser => parsedUrl.href.indexOf(badgeParser) !== -1);
+
+    if (!isBadgeUrl(url) || !badgeParser) {
+        return null;
+    }
+
+    return parsers[badgeParser](parsedUrl);
+}
+
+module.exports = readme => {
+    return extractUrls(readme)
+        .map(parseBadgeUrl)
+        .filter(badge => !!badge);
+};
